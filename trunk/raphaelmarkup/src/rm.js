0c9dd89a05951f6ce3ab2817d9c966af28e254b1
sebastigurin@gmail.com <sebastigurin@gmail.com@6cee721c-9732-ee2e-279f-6c1ca05eb236>
2012-03-09 22:12:01 +0000

    update functions and fixes

--- trunk/raphaelmarkup/src/rm.js
+++ trunk/raphaelmarkup/src/rm.js
@@ -16,9 +16,19 @@
 
 
 
+
 var rm = {
-		
-		
+_doLog: true, 
+_log : function(s) {
+	if(rm._doLog) {
+		if($("#rmlogger").size()==0) {
+//			debugger;
+			$(document.body).append(
+				"<textarea id=\"rmlogger\" style=\"width: 100%; height: 100px\">hello</textarea>");
+		}
+		$("#rmlogger").text($("#rmlogger").text()+"\n"+s);
+	}
+},
 		
 		
 		
@@ -44,22 +54,38 @@ var rm = {
 
 /**
  * render all <raphael elements in current document
+ * @return an array of <raphael doms (jquery objects)
  */
 render: function() {
-	$("raphael").each(function(index){
-		var rdoc= rm.buildRDocFromDOM($(this));
+	var docs = [];
+	$("raphael").each(function(index) {
+		var rdoc = rm.buildRDocFromDOM($(this));
+		docs.push(rdoc);
 		rm._render(document, rdoc);
 	});	
+	return docs;
 },
-
-renderAjax: function(url, id) {
+/**
+ * get an xml document with a request (must be local server uri). renders it in
+ * curent document. Use like:
+<pre>rm.renderAjax("drawing1.xml", function(raphael) {
+	raphael.find(".set1 circle").attr("radius", "300");
+}); </pre>
+ * @param successHandler a function that accept one parameter, the jquery dom object for <raphael>
+ * 
+ */
+renderAjax: function(url, successHandler, errorHandler) {
 	
 	$.ajax({
 		"url": url,
 		"success": function(data) {
 			var rdoc= $(data); 
+//			var fragment = $(document.createDocumentFragment())
+//			$(document.body).append(fragment);
 			rm._render(document, rdoc);
+			successHandler(rdoc);
 		},
+		"error": errorHandler,
 		"dataType": "xml"
 	});
 },
@@ -119,7 +145,7 @@ buildRDocFromDOM : function(dom) {
 _render : function(targetHtmlDoc, rdoc)	{
 	
 	/* first of all try to hide the <raphael> element from html document */
-	rdoc.hide();
+//	rdoc.hide();
 	
 	/* first parse each <style> element against document. 
 	 * the DOM will be affected before rendering.*/
@@ -135,21 +161,56 @@ _render : function(targetHtmlDoc, rdoc)	{
 		
 	rdoc.find("paper").each(function(i){
 		var paper = null;
-		if($(this).attr("containerId")) {
-			paper = Raphael($(this).attr("containerId"), 
-				parseInt($(this).attr("width")), parseInt($(this).attr("height")));
-		}
-		else {
-			paper = Raphael(parseInt($(this).attr("x")), parseInt($(this).attr("y")), 
-				parseInt($(this).attr("width")), parseInt($(this).attr("height")));
-		}
-		rm._register("paper", paper, $(this));
-		rm._renderPaper($(this), paper);
-	});
-	
+		
+		rm._renderPaper(rdoc, $(this), paper);
+	}); 
 	
 	
 	return rdoc;
+},
+/**
+ * updates dthe UI of element dom in document rdoc.
+ * it will eliminates the shape/paper dom, and it will recreate it.
+ * TODO. this could be more efficient
+ */
+update : function(rdoc, dom) {
+	if(!dom) //update all papers
+		dom = rdoc.find("paper");
+	dom.each(function(index) {
+		rm.updateShape(rdoc, $(this));
+	});
+},
+/** updates a single shape and its childs */
+updateShape : function(rdoc, dom) {
+	var id = dom.attr("id"), 
+		shape = rm.getShapeById(id), 
+		type = null;
+	
+	if(!shape) { /* the user send a new shape to update, we update its parent. */
+		dom = dom.parent();
+		id = dom.attr("id");
+		shape = rm.getShapeById(id);
+		type = shape.type;
+	}
+	else
+		type = shape.type;
+	
+	if(!shape || !shape.type)
+		return;
+	
+	rm._unregister(rdoc, dom, shape);
+	shape.remove();//removes paper, set or shapes from HTML DOM
+	
+	rm._log("updateShape: type: "+type+" - id: "+dom.attr("id"));
+	
+	if(type=="paper") {		
+		rm._renderPaper(rdoc, dom);
+	}
+	else { //it is a shape or set
+		var paperId = dom.parent("paper").attr("id");
+		var paper = rm.getShapeById(paperId);
+		rm._renderEl(rdoc, dom, paper);
+	}
 },
 /**
  * this apply a css source string in a DOM.
@@ -158,46 +219,83 @@ applyCSS: function(doc, cssStr) {
 	var css = rm.parseCSS(cssStr);
 	for ( var i = 0; i < css.__order.length; i++) {
 		var sel = css.__order[i];
-//		alert(sel+" - "+doc.find(sel).size());
-//		debugger;
 		var el = doc.find(sel);
-//		el.parents("set").each(function(index){
-//			
-//		});
 		el.attr(css[sel]);
 	}
-//	for(var sel in css) {}
+},
+/**
+ * create a new element in raphael XML dom
+ * @param parent an html dom or jquery selector or object where to append the created child. if parent reffers to a XMLDocument then the created element won't be appended top anything.
+ * @return the created jquery dom element 
+ */
+createElement: function(parent, tagName, attrs) {
+//	var rdoc = null;
+	var xmldoc = null, isdoc = false;
+	
+	if($(parent).get(0).createElement) {
+		xmldoc=$(parent).get(0);
+		isdoc = true;
+	}	
+	else
+		xmldoc=$(parent).prop("ownerDocument");
+	var e = $(xmldoc.createElement(tagName));
+	if(attrs)
+		e.attr(attrs);
+	if(!isdoc)
+		$(parent).append(e);
+	return e;
 },
 /**
  * @param el jquery object with paper xml dom
  * @param paper - the raphael paper object
  */
-_renderPaper: function(el, paper) {
-//	alert("children: "+el.children().size())
-	el.children().each(function(index){
-		rm._renderEl($(this), paper);
+_renderPaper: function(rdoc, dom) {
+	var paper = null;
+	if(dom.attr("containerId")) {
+		paper = Raphael(dom.attr("containerId"), 
+			parseInt(dom.attr("width")), parseInt(dom.attr("height")));
+	}
+	else {
+		paper = Raphael(parseInt(dom.attr("x")), parseInt(dom.attr("y")), 
+			parseInt(dom.attr("width")), parseInt(dom.attr("height")));
+	}
+	
+	dom.children().each(function(index){
+		rm._renderEl(rdoc, $(this), paper);
 	});
+	paper.type="paper";
+	rm._register(rdoc, "paper", paper, dom);	
+	rm._registerMutationEvents(rdoc, dom, paper);
+	return paper;
 },
 /**
  * @param el the jquery object for xml dom el def
  * @param paper the raphael paper 
  */
-_renderEl: function(el, paper) {
+_renderEl: function(rdoc, dom, paper) {
 	
 	var shape = null;
-	if(el.get(0).tagName.toLowerCase()=="image")
-		shape=this._renderImage(el, paper);
-	else if(el.get(0).tagName.toLowerCase()=="rect")
-		shape=this._renderRect(el, paper);
-	else if(el.get(0).tagName.toLowerCase()=="path")
-		shape=this._renderPath(el, paper);
-	else if(el.get(0).tagName.toLowerCase()=="set")
-		shape=this._renderSet(el, paper);
-	else if(el.get(0).tagName.toLowerCase()=="circle")
-		shape=this._renderCircle(el, paper);	
-	else if(el.get(0).tagName.toLowerCase()=="ellipse")
-		shape=this._renderEllipse(el, paper);	
+	if(rm._getTagName(dom)=="image")
+		shape=this._renderImage(dom, paper);
+	else if(rm._getTagName(dom)=="rect")
+		shape=this._renderRect(dom, paper);
+	else if(rm._getTagName(dom)=="path")
+		shape=this._renderPath(dom, paper);
+	else if(rm._getTagName(dom)=="set")
+		shape=this._renderSet(rdoc, dom, paper);
+	else if(rm._getTagName(dom)=="circle")
+		shape=this._renderCircle(dom, paper);	
+	else if(rm._getTagName(dom)=="ellipse")
+		shape=this._renderEllipse(dom, paper);
+	
+	if(shape!=null) {
+		rm._register(rdoc, shape.type, shape, dom);
+		rm._registerMutationEvents(rdoc, dom, shape);
+	}
 	return shape;
+},
+_getTagName: function(dom) {
+	return dom.get(0).tagName.toLowerCase()
 },
 /**
  * extracts xml raphael attributes to a js object
@@ -214,38 +312,40 @@ getRaphaelAttrs: function(el) {
 /**do the renderization of attributes defined in xml dom object to a raphael shape
  * @param el - the jquery xml dom of an element
  * @param shape - he raphael shape that presents the element
- * @param paper - the raphael paper.
+ * @param paper - optional the raphael paper.
  */
-_renderAttrs : function(el, shape, paper) {	
-	var attrs = rm.getRaphaelAttrs(el);
-//	alert("_renderAttrs: "+el.attr("id")+" - "+rm._dump(attrs));
-//	debugger;
-	shape.attr(attrs);
+_renderAttrs : function(dom, shape, paper) {
+	if(shape) {
+		if(!paper)
+			paper = shape.paper;
+		var attrs = rm.getRaphaelAttrs(dom);
+		shape.attr(attrs);
+	}
 },
-_renderImage: function(el, paper) {
-	var shape = paper.image(el.attr("src"), el.attr("x"), 
-		el.attr("y"), el.attr("width"), el.attr("height"));
-	this._renderAttrs(el, shape, paper);
+_renderImage: function(dom, paper) {
+	var shape = paper.image(dom.attr("src"), dom.attr("x"), 
+		dom.attr("y"), dom.attr("width"), dom.attr("height"));
+	this._renderAttrs(dom, shape, paper);
 	return shape;
 },
-_renderEllipse: function(el, paper) {
-	var shape =  paper.ellipse(el.attr("x"), el.attr("y"), 
-		el.attr("rx"), el.attr("ry"));
-	this._renderAttrs(el, shape, paper);
+_renderEllipse: function(dom, paper) {
+	var shape =  paper.ellipse(dom.attr("x"), dom.attr("y"), 
+		dom.attr("rx"), dom.attr("ry"));
+	this._renderAttrs(dom, shape, paper);
 	return shape;
 },
-_renderRect: function(el, paper) {
-	var shape =  paper.rect(el.attr("x"), el.attr("y"), 
-		el.attr("width"), el.attr("height"), el.attr("radius"));
-	this._renderAttrs(el, shape, paper);
+_renderRect: function(dom, paper) {
+	var shape =  paper.rect(dom.attr("x"), dom.attr("y"), 
+		dom.attr("width"), dom.attr("height"), dom.attr("radius"));
+	this._renderAttrs(dom, shape, paper);	
 	return shape;
 },
-_renderPath: function(el, paper) {
-	var shape = paper.path(el.attr("path"));
-	this._renderAttrs(el, shape, paper);
+_renderPath: function(dom, paper) {
+	var shape = paper.path(dom.attr("path"));
+	this._renderAttrs(dom, shape, paper);
 	return shape;
 },
-_renderSet: function(el, paper) {
+_renderSet: function(rdoc, dom, paper) {
 	/* note that we first render set attrs, and then render set's children attrs, so one can do in CSS: 
 	.set1 {
 		fill: yellow;
@@ -255,32 +355,90 @@ _renderSet: function(el, paper) {
 	}
 	 */
 	var set = paper.set();
+	//set.paper = paper; //set lacks of paper reference
 	
 	//one iteration for building the set.
-	el.children().each(function(index){		
-		var childShape = rm._renderEl($(this), paper);
+	dom.children().each(function(index){		
+		var childShape = rm._renderEl(rdoc, $(this), paper);
 		set.push(childShape);
 	});
 	//render the set attributes
-	rm._renderAttrs(el, set, paper);
+	rm._renderAttrs(dom, set, paper);
 	//other iteration for setting attributes of children (after set attrs )
-	el.children().each(function(index){	
+	dom.children().each(function(index){	
 		var shape = set[index];
+
+//		if(!shape)
+//			debugger;
 //		//first stylyze according to parent set
-//		rm._renderAttrs(el, shape, paper);
+//		rm._renderAttrs(dom, shape, paper);
 		//and then apply specific style
 		rm._renderAttrs($(this), shape, paper);
 	});	
 	return set;
 },
-_renderCircle: function(el, paper) {
-	var shape =  paper.circle(el.attr("cx"), el.attr("cy"), 
-		el.attr("radius"));
-	this._renderAttrs(el, shape, paper);
+_renderCircle: function(dom, paper) {
+	var shape =  paper.circle(dom.attr("cx"), dom.attr("cy"), 
+		dom.attr("radius"));
+	this._renderAttrs(dom, shape, paper);
 	return shape;
 },	
-
-
+//_mutationHandlers: {},
+/*
+DOMSubtreeModified 	(none) 	Fires when the subtree is modified 	Yes 	No
+DOMNodeInserted 	(none) 	Fires when a node has been added as a child of another node 	Yes 	No
+DOMNodeRemoved 	(none) 	Fires when a node has been removed from a DOM-tree 	Yes 	No
+DOMNodeRemovedFromDocument 	(none) 	Fires when a node is being removed from a document 	No 	No
+DOMNodeInsertedIntoDocument 	(none) 	Fires when a node is being inserted into a document 	No 	No
+DOMAttrModified 	(none) 	Fires when an attribute has been modified 	Yes 	No
+DOMCharacterDataModified 	(none) 	Fires when the character data has been modified 	Yes 	No*/
+_registerMutationEvents: function(rdoc, dom, shape) {	
+//	rm._log("_registerMutationEvents for "+dom.attr("id"));
+//	
+//	/* DOMAttrModified - Fires when an attribute has been modified - 
+//	 * here we only update the attributes of that shape. in case class or id change, 
+//	 * there we reapply all styles from this element in the DOM.
+//	 */
+//	var attrModifiedHandler = function(evt) {
+//		evt=rm._fixEvent(evt);
+//		/* note in FF 
+//		 * evt.attrName indicates the attribute name changed
+//		 * evt.originalEvent.newValue and evt.originalEvent.prevValue
+//		 */
+//		var id = $(evt.target).attr("id");
+//		var shape = rm.getShapeById(id);
+//		var doc = rm.getDocById(id), dom = doc.find("#"+id);
+//		rm._log("DOMAttrModified id. "+id+" - domcount: "+dom.size()+
+//				" - shape: "+shape+
+//				" - prevValue: "+evt.originalEvent.prevValue+
+//				" - newValue: "+evt.originalEvent.newValue);		
+//		if(shape) {//shape isn't created yet			
+//			rm._renderAttrs(dom, shape);
+//		}
+//	};
+//	
+//	rm._registerDOMAttrModified(dom, attrModifiedHandler);
+},
+//_registerDOMAttrModified: function(dom, callback) {
+//	if($.browser.webkit) {
+//		/* WebKit fails to fire DOMAttrModified events when an attribute is changed. 
+//		 * It does however fire the DOMSubtreeModified event after an attribute 
+//		 * is modified. So at least that gives us something to work with until 
+//		 * the good folks at WebKit squash the bug.*/
+////		dom.bind("DOMSubtreeModified", callback); //TODO: fix evt object
+//		dom.bind("DOMSubtreeModified", function(evt) {
+////			evt.type=""
+//			alert("DOMSubtreeModified");
+//		}); 
+//	}
+//	else {
+//		dom.bind("DOMAttrModified", attrModifiedHandler);
+//	}
+//},
+//_fixEvent: function(e) {
+//	e.newValue=e.originalEvent.newValue;
+//	e.prevValue=e.originalEvent.prevValue;
+//},
 
 
 
@@ -379,24 +537,54 @@ _xmlGetId: function(el) {
 
 	/* * * * object registration and raphael constants * * * */
 
-_els : {},
+//_els : {},
+_shapes: {},
+_docsById: {},
 /**@param type string - the type of element to register "paper", "set", image", etc
  * @param el - raphael shape or paper object
  * @param domEl the xml dom of the element 
  */
-_register: function(type, el, domEl) {
-	if(!rm._els[type])
-		rm._els[type]={};
-	rm._els[type][rm._getId(type, el, domEl)] = el;
+_register: function(rdoc, type, el, domEl) {
+	if(!type)
+		type = el.type?el.type:"paper";
+	if(el && domEl) {
+		var id = rm._getId(type, el, domEl);		
+		rm._shapes[id]=el;
+		rm._docsById[id]=rdoc;
+		rm._log("register "+el.type+": "+id);
+		domEl.attr("id", id);
+	}
 },
-_ids : {},		
+_unregister: function(rdoc, dom, shape) {
+	var id = dom.attr("id");
+	if(rm._shapes[id])
+		delete rm._shapes[id];
+	if(rm._docsById[id])
+		delete rm._docsById[id]
+	rm._log("unregistered "+shape.type+": "+id);
+},
+getDocById: function(id) {
+	return rm._docsById[id];
+},
+///**
+// * reguster a dom element for mutation events, so if the dom change, 
+// */
+//_registerMutationEvents: function(type, id, el, domEl) {
+//	
+//},
+_elId: 0,
+getShapeById: function(id) {
+	return rm._shapes[id];
+},
+//_ids : {},		
 _getId: function(type, el, domEl) {
-	if(domEl.attr("id"))
+	if(domEl.attr("id")&&domEl.attr("id")!="")
 		return domEl.attr("id");
 	else {
-		rm._ids++;
-		var id = type+"_"+rm._ids;
+		rm._elId++;
+		var id = type+"_"+rm._elId;
 		domEl.attr("id", id);
+		return id;
 	}
 },
 _dump: function(o) {
