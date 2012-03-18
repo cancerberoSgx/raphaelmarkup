6920fa6cf0bfdfb09995c83cbdeb4cef921ca702
sebastigurin@gmail.com <sebastigurin@gmail.com@6cee721c-9732-ee2e-279f-6c1ca05eb236>
2012-03-18 16:51:13 +0000

    print and text separated elements, print onpath attribute extension, website made with rm started

--- src/makeDist.sh
+++ src/makeDist.sh
@@ -1,8 +1,21 @@
+VERSION=1.0
+DISTNAME=raphaelmarkup-$VERSION
+
+#sh makejs.sh
+
 cd ..
 rm -rf dist
-svn export . dist
-cd dist/src
+mkdir dist
+svn export . dist/$DISTNAME
+cd dist/$DISTNAME/src
 
 cat rm-core.js > rm.js
 cat rm-ext.js >> rm.js
-java -jar ../../yuicompressor-2.4.7.jar -v -o rm-min.js rm.js 
+java -jar ../../../yuicompressor-2.4.7.jar -v -o rm-min_.js rm.js 
+echo "/* raphaelmarkup - http://code.google.com/p/raphaelmarkup/ - by Sebastián Gurin - MIT LICENSE */"> rm-min.js
+cat rm-min_.js >> rm-min.js
+rm -r rm-min_.js
+
+cd ../.. #dist
+tar cvfz $DISTNAME.tar.gz $DISTNAME
+rm -rf $DISTNAME
